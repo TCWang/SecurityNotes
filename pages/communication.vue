5c652e8d0b2b5e18b77333e226dc63774efd5413
TC Wang <wangtct@hotmail.com>
2024-03-16 15:12:39 +0800

    eng

--- pages/communication.vue
+++ pages/communication.vue
@@ -4,15 +4,23 @@
     <ul>
       <li>
         1. 先確認公司標的及營運模式: 有哪些公司會招聘 資訊安全工程師？
-        是資訊安全專業公司，還是大公司自行建構的資安部門？ <a href="">SOC</a>
+        是資訊安全專業公司，還是大公司自行建構的資安部門？
+        <a href="https://securitynotes.netlify.app/soc">SOC</a>
       </li>
       <li>
         2. 確認尋求職業 Job Title, 各公司可能有不同的定義。但需要找出較通用的！
-        <a href="">Job Definition</a>
+        譬如 Detection Engineer 是否會出現在職缺 分類中？
+
+        <a href="https://securitynotes.netlify.app/job">Job Definition</a>
       </li>
       <li>
         3. 招娉網如 Linkedin, 評估市場需求及機會有多少。
-        <a href="">Oppertunity</a>
+        <a href="https://securitynotes.netlify.app/opp">Oppertunity</a>
+      </li>
+      <li>
+        4. 依據 Job Title, 歸納出此職系所需的技能。 如Detection Engineer
+        需要的技能要求有哪些
+        <a href="https://securitynotes.netlify.app/eng">Detection Engineer</a>
       </li>
     </ul>
   </div>
